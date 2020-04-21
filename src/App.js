fd49b6d7d5ece234f99e5b41a8c3c75806145dee
user <mattjackmcgregor@gmail.com>
2020-04-21 16:59:14 +1200

    fetching full data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import './App.css';
+
+import {Cards, CountryPicker, Chart} from './components'
+import style from './App.module.css'
+ import {fetchData} from './api'
 
 export class App extends React.Component {
+
+  //how to use async with componentdidmount
+  async componentDidMount () {
+    const data = await fetchData()
+    console.log(data)
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className={style.App}>
        <h1>covid 19 tracker</h1>
+       <Cards />
+       <CountryPicker />
+       <Chart />
       </div>
     )
   }
